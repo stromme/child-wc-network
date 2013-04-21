5899115f64a1d39bf6a665a2382e68e76989d8c7
Lars <lars@stromme.ca>
2013-04-21 07:45:56 +0545

    Add dynamic title to headline

--- templates/menu-headline.php
+++ templates/menu-headline.php
@@ -7,8 +7,12 @@
  * @subpackage 
  * @since 
  */
-
+global $post;
 $seo = get_location_seo();
+
+// Get the service type being viewed if we're on a services page and put it in the H1 element.
+$title = ( ( 'cftl-tax-landing' == get_post_type() ) ? 'North America\'s <b> Best '.ucwords($post->post_title).'</b>' : 'North America\'s <b> Best Window Cleaning</b> Professionals.' );
+
 ?>
 
 <!-- Headline - Fixed to top of page
@@ -16,10 +20,10 @@ $seo = get_location_seo();
 -->
 
 <header class="container">
-	<div class="headline headline-1200">
+	<div class="headline">
 		<ul>
 			<li class="headline-title">
-				<h1 itemprop="description">North America's <b>Best Window Cleaning Professionals. </b></h1>
+				<h1 itemprop="description"><?=$title?></h1>
 			</li>
 			<li class="headline-phone">
 				<?php $tb_company = get_option('tb_company'); ?>
