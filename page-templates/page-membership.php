81bdeb8a4c6e52511be46de1363f01e045890c89
Lars <lars@stromme.ca>
2013-05-01 11:47:25 +0545

    Apply Modal

--- page-templates/page-membership.php
+++ page-templates/page-membership.php
@@ -9,42 +9,61 @@
  */
  
 get_header();
+the_post();
+the_content();
 ?>
 
-<section class="container gentle-shadow bg-white bumper-bottom-medium">
-	<div class="page-left page-right bumper-top-medium center">
-		<div class="row-fluid">
-			<div class="offset1 span10">
-				<div class="select-location">
-					<h2>I need a great window cleaner in... </h2>
-					<span class="btn-group">
-				  		<a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">State <span class="caret"></span></a>
-				  		<ul class="dropdown-menu">
-				    		<li><a href="">Canada</a></li>
-				    		<li><a href="">United States</a></li>
-				  		</ul>
-					</span>
-					<span class="btn-group">
-						<a class="btn dropdown-toggle btn-large" data-toggle="dropdown" href="#">United States <span class="caret"></span></a>
-						<ul class="dropdown-menu">
-							<li><a href="">Canada</a></li>
-							<li><a href="">United States</a></li>
-						</ul>
-					</span>
-					<button class="btn btn-large btn-success">Go!</button>
-					<p class="muted"><small>Recommended by customers in over 900 cities.</small></p>
-				</div>
-				<ul class="city-list">
-					<li><a href="#" class="label-city"><h3>San Francisco</h3></a></li>	
-					<li><a href="#" class="label-city"><h3>San Francisco</h3></a></li>	
-					<li><a href="#" class="label-city"><h3>San Francisco</h3></a></li>	
-					<li><a href="#" class="label-city"><h3>San Francisco</h3></a></li>
-				</ul>		
-			</div>
-	</div>
-	</div>
-</section>
+<!-- Lead Modal -->
+<div class="big-modal">
+  <div class="modal hide fade bg-slate" id="member-application">
+    <div class="modal-header center bumper-top bumper-bottom">
+      <h3><strong class="green">100% refund</strong> within 60 days if you don’t love being a member.</h3>
+    </div>
+    <div class="modal-body center bumper-top">
+      <div>
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-user"></i></span>
+            <input validation="not-empty" data-field-name="Name" placeholder="Your Name" type="text" value="" />
+        </div>
+      </div>
+      <div>
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-breifcase"></i></span>
+            <input validation="not-empty" placeholder="Company Name" type="text" value="" />
+        </div>
+      </div>
+      <div>
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-map-marker"></i></span>
+            <input validation="not-empty" placeholder="City" type="text" value="" />
+        </div>
+      </div>
+      <div>
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-envelope"></i></span>
+            <input validation="not-empty email" placeholder="Email Address" type="text" value="" />
+        </div>
+      </div>
+      <div>
+        <div class="input-prepend">
+            <span class="add-on"><i class="icon-phone-halfling"></i></span>
+            <input  validation="not-empty phone" placeholder="Phone Number" type="text" value="" />
+        </div>
+      </div>
+      <div class="bumper-top-small bumper-bottom">
+        <div class="pen-stroke"></div>
+      </div>
+      <p class="page-left page-right">One of our team members will contact you right away.</p>
 
+
+    </div>
+
+    <div class="modal-footer center">
+      <a href="" class="btn btn-large btn-success save">Apply now</a>
+    </div>
+  </div>
+</div>
+<!-- / modal -->
 
 
 
