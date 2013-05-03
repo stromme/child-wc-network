64ad71b961b0c3147b1ff77d354a88a0319fb9f0
Lars <lars@stromme.ca>
2013-05-03 21:38:36 +0545

    url

--- page-templates/page-awards-badge.php
+++ page-templates/page-awards-badge.php
@@ -10,7 +10,7 @@ if (isset($_REQUEST['award'])) {
 	?>
 	<div style="width:285px; height: 80px; margin: 5px;background-image:url(<?php bloginfo('template_directory') ?>/images/window-cleaning.png);">
 		<a href="<?php echo network_site_url().$city; ?>" target="_blank" style="text-decoration:none;"><h3 style="padding: 10px 0px 0px 50px; margin: 0px; color: #ffffff;font: normal 15px Arial, sans-serif;"><?php echo $cityUpper; ?></h3><h2 style="margin: 0px; padding: 0px 0px 0px 50px; color: #ffffff; font: bold 15px Arial, sans-serif;">2013 BEST WINDOW CLEANING</h2></a>
-		<a href="http://www.windowcleaning.com" target="_blank"><img style="padding: 0px 0px 0px 47px;"src="<?php bloginfo('template_directory') ?>/images/best-window-cleaners.png" alt="" />
+		<a href="http://www.windowcleaning.com" target="_blank"><img style="padding: 0px 0px 0px 47px;"src="<?php echo get_template_directory_uri(); ?>/images/best-window-cleaners.png" alt="" />
 		</a>
 	</div>
 	<?php
