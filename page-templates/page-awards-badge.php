6404d14924402969aa7757c34e1d29efda67f8aa
Lars <lars@stromme.ca>
2013-05-03 21:20:13 +0545

    Award Badge

---
+++ page-templates/page-awards-badge.php
@@ -0,0 +1,18 @@
+<?php 
+/*
+Template Name: Award Badge
+*/
+//send iframe award
+if (isset($_REQUEST['award'])) {
+	$city = $_REQUEST['award'];
+	$cityUpper=str_replace('-', ' ', strtoupper($city));
+	$city=strtolower($city);
+	?>
+	<div style="width:285px; height: 80px; margin: 5px;background-image:url(<?php bloginfo('template_directory') ?>/images/window-cleaning.png);">
+		<a href="<?php echo network_site_url().$city; ?>" target="_blank" style="text-decoration:none;"><h3 style="padding: 10px 0px 0px 50px; margin: 0px; color: #ffffff;font: normal 15px Arial, sans-serif;"><?php echo $cityUpper; ?></h3><h2 style="margin: 0px; padding: 0px 0px 0px 50px; color: #ffffff; font: bold 15px Arial, sans-serif;">2013 BEST WINDOW CLEANING</h2></a>
+		<a href="http://www.windowcleaning.com" target="_blank"><img style="padding: 0px 0px 0px 47px;"src="<?php bloginfo('template_directory') ?>/images/best-window-cleaners.png" alt="" />
+		</a>
+	</div>
+	<?php
+}
+?>
