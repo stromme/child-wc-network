5fe0477196fb1ae0e27157466b82e2b8ff9a5699
Josua Leonard <josua.leonard@gmail.com>
2014-04-26 00:12:57 +0700

    Fix locations

--- functions.php
+++ functions.php
@@ -105,10 +105,9 @@ function select_windowcleaning_location(){
     if(count($results)>0){
       foreach($results as $result){
         $details = get_blog_details( $result->blog_id, false );
-        echo "<!-- ";
-        var_dump($details);echo " -->";
-
-        echo '<li><a href="'.get_blogaddress_by_id($result->blog_id).'" class="label-city"><h3>'.$result->city.'</h3>'.($delete?'.':'').'</a></li>';
+        if(!isset($details->deleted) && !$details->deleted){
+          echo '<li><a href="'.get_blogaddress_by_id($result->blog_id).'" class="label-city"><h3>'.$result->city.'</h3>'.($delete?'.':'').'</a></li>';
+        }
       }
     }
     else {
